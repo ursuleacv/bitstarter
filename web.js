128c6671cac8d23a117a156886b417ae4f3e0208
Valentin Ursuleac <ursuleacv@gmail.com>
2013-07-23 12:50:25 -0400

    Added skeleton

--- web.js
+++ web.js
@@ -1,12 +1,15 @@
 var express = require('express');
-var app = express();
-app.use(express.logger());
+var fs = require('fs');
+var home = 'index.html'
+
+var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+    var buffer = new Buffer(fs.readFileSync(home), "utf-8");
+    response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
